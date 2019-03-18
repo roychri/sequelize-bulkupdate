c8c2457e8a6bfeb7cabb515e8178f4c13835d572
SalathielGenese <salathielgenese@gmail.com>
2019-03-18 17:53:51 +0100

    refactor test to improe readability

--- spec/integration/sequelize-bulkupdate:postgres.spec.js
+++ spec/integration/sequelize-bulkupdate:postgres.spec.js
@@ -41,39 +41,57 @@ describe( 'sequelizeBulkUpdate', () =>
 
         it( 'by id (default)', async () =>
         {
+            const LENGTH = 2;
             const returning = true;
-            const tickets = [ ...Array( 2 ) ].map( () => getUN() );
-            const visitors = ( await Visitor.bulkCreate( tickets.map( () =>
-                ({ name: uuid(), ticket: getUN() }) ), { returning }) ).map( ({ dataValues }) => dataValues );
+            const names = [ ...Array( LENGTH ) ].map( () => uuid() );
+            const tickets = [ ...Array( LENGTH ) ].map( () => getUN() );
+            const initials = [ ...Array( LENGTH ) ].map( () => ({ name: uuid(), ticket: getUN() }) );
+            const persisted = ( await Visitor.bulkCreate( initials, { returning }) ).map( ({ dataValues }) => dataValues );
 
-            await Visitor.bulkUpdate( visitors.map( ({ id }, index ) => ({ id, ticket: tickets[ index ] }) ));
+            for ( const visitor of persisted ) {
+                const initial = initials.find( ({ name }) => name === visitor.name );
 
-            const BULK_UPDATED = ( await Visitor.findAll({ where: { id: visitors.map( ({ id }) => id ) } }) ).map( visitor =>
-                [ visitor, visitors.findIndex( ({ id }) => id === visitor.id ) ] ).map( ([ visitor, index ]) =>
-                    [ visitor, visitors[ index ], tickets[ index ] ]).every( ([
-                        { id, name, ticket }, { id: _id, name: _name, ticket: _ticket }, ticketx
-                    ]) => +_id === +id && _name === name && +_ticket !== +ticket && +ticket === +ticketx );
+                initial.id = visitor.id;
+            }
 
-            expect( BULK_UPDATED ).toBe( true );
+            const differences = persisted.map( ({ id }, index ) => ({ id, name: names[ index ], ticket: tickets[ index ] }) );
+            const [ updates ] = await Visitor.bulkUpdate( differences, { returning });
+
+            for ( let i = 0, l = persisted.length, name, current, updated; i < l; i++ ) {
+                current = persisted[ i ];
+                updated = updates.find( ({ id }) => id === current.id );
+                name = names[ initials.findIndex( ({ id }) => id === current.id ) ];
+                ticket = tickets[ initials.findIndex( ({ id }) => id === current.id ) ];
+
+                expect( updated.name ).toBe( name );
+                expect( updated.id ).toBe( current.id );
+                expect( +updated.ticket ).toBe( ticket );
+                expect( updated.name ).not.toBe( current.name );
+                expect( +updated.ticket ).not.toBe( +current.ticket );
+            }
         });
 
         it( 'by custom key', async () =>
         {
+            const LENGTH = 2;
             const key = 'ticket';
             const returning = true;
-            const names = [ ...Array( 2 ) ].map( () => uuid() );
-            const visitors = ( await Visitor.bulkCreate( names.map( () =>
-                ({ name: uuid(), ticket: getUN() }) ), { returning }) ).map( ({ dataValues }) => dataValues );
+            const names = [ ...Array( LENGTH ) ].map( () => uuid() );
+            const initials = [ ...Array( LENGTH ) ].map( () => ({ name: uuid(), ticket: getUN() }) );
+            const persisted = ( await Visitor.bulkCreate( initials, { returning }) ).map( ({ dataValues }) => dataValues );
+            const differences = persisted.map( ({ ticket }, index ) => ({ ticket, name: names[ index ] }) );
+            const [ updates ] = await Visitor.bulkUpdate( differences, { returning, key });
 
-            await Visitor.bulkUpdate( visitors.map( ({ ticket }, index ) => ({ ticket, name: names[ index ] }) ), { key });
+            for ( let i = 0, l = persisted.length, name, current, updated; i < l; i++ ) {
+                current = persisted[ i ];
+                updated = updates.find( ({ id }) => id === current.id );
+                name = names[ initials.findIndex( ({ name }) => name === current.name ) ];
 
-            const BULK_UPDATED = ( await Visitor.findAll({ where: { id: visitors.map( ({ id }) => id ) } }) ).map( visitor =>
-                [ visitor, visitors.findIndex( ({ id }) => id === visitor.id ) ] ).map( ([ visitor, index ]) =>
-                    [ visitor, visitors[ index ], names[ index ] ]).every( ([
-                        { id, name, ticket }, { id: _id, name: _name, ticket: _ticket }, namex
-                    ]) => +_id === +id && +_ticket === +ticket && _name !== name && name === namex );
-
-            expect( BULK_UPDATED ).toBe( true );
+                expect( updated.name ).toBe( name );
+                expect( updated.id ).toBe( current.id );
+                expect( updated.name ).not.toBe( current.name );
+                expect( +updated.ticket ).toBe( +current.ticket );
+            }
         });
 
         it( 'only specific fields', async () =>
@@ -83,18 +101,23 @@ describe( 'sequelizeBulkUpdate', () =>
             const fields = [ 'ticket' ];
             const names = [ ...Array( LENGTH ) ].map( () => uuid() );
             const tickets = [ ...Array( LENGTH ) ].map( () => getUN() );
-            const visitors = ( await Visitor.bulkCreate( names.map( () =>
-                ({ name: uuid(), ticket: getUN() }) ), { returning }) ).map( ({ dataValues }) => dataValues );
+            const initials = tickets.map( () => ({ name: uuid(), ticket: getUN() }) );
+            const persisted = ( await Visitor.bulkCreate( initials, { returning }) ).map( ({ dataValues }) => dataValues );
+            const differences = persisted.map( ({ id }, index ) => ({ id, name: names[ index ], ticket: tickets[ index ] }) );
+            const [ updates ] = await Visitor.bulkUpdate( differences, { returning, fields });
 
-            await Visitor.bulkUpdate( visitors.map( ({ id }, index ) => ({ id, ticket: tickets[ index ], name: names[ index ] }) ), { fields });
+            for ( let i = 0, l = persisted.length, name, ticket, current, updated; i < l; i++ ) {
+                current = persisted[ i ];
+                updated = updates.find( ({ id }) => id === current.id );
+                name = names[ initials.findIndex( ({ name }) => name === current.name ) ];
+                ticket = tickets[ initials.findIndex( ({ name }) => name === current.name ) ];
 
-            const BULK_UPDATED = ( await Visitor.findAll({ where: { id: visitors.map( ({ id }) => id ) } }) ).map( visitor =>
-                [ visitor, visitors.findIndex( ({ id }) => id === visitor.id ) ] ).map( ([ visitor, index ]) =>
-                    [ visitor, visitors[ index ], tickets[ index ] ]).every( ([
-                        { id, name, ticket }, { id: _id, name: _name, ticket: _ticket }, ticketx
-                    ]) => +_id === +id && _name === name && +_ticket !== +ticket && +ticket === +ticketx );
-
-            expect( BULK_UPDATED ).toBe( true );
+                expect( updated.id ).toBe( current.id );
+                expect( updated.name ).not.toBe( name );
+                expect( +updated.ticket ).toBe( +ticket );
+                expect( updated.name ).toBe( current.name );
+                expect( +updated.ticket ).not.toBe( +current.ticket );
+            }
         });
 
         it( 'return updated values if { returning: true } option is provided', async () =>
